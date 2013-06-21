1592c5a21a81b9bcab0e799c500433d88453cdc9
Ilya Bylich <ilya.bylich@productmadness.com>
2013-06-21 22:15:30 +0300

    Specs refactoring.

--- spec/hashifiable_spec.rb
+++ spec/hashifiable_spec.rb
@@ -2,28 +2,32 @@ require 'spec_helper'
 
 class TestUser < Struct.new(:id, :name, :credit_card, :secret_token)
   extend Hashifiable
-  hashify :id, :name, :two_times_two => Proc.new { 2 * 2 }, :encrypted_token => Proc.new { secret_token + ' secret sauce' }, lambdas_at_work: ->() { 2 * 2 }
+  hashify :id,
+          :name,
+          :two_times_two => Proc.new { 2 * 2 },
+          :encrypted_token => Proc.new { secret_token + ' secret sauce' },
+          :lambdas_at_work => ->() { 2 * 2 }
 end
 
 describe Hashifiable do
   let(:user) { TestUser.new(1, 'pote', '1123123241241', 'i2j34i2j34302843') }
 
   it 'should have a to_h method' do
-    user.respond_to?(:to_h).should be_true
+    user.should respond_to(:to_h)
   end
 
   it 'support to_hash for old fashioned people' do
-    user.respond_to?(:to_hash).should be_true
+    user.should respond_to(:to_hash)
   end
 
   it 'should have a hash representation of the desired attributes' do
-    user.to_h.keys.include?(:id).should be_true
-    user.to_h.keys.include?(:name).should be_true
+    user.to_h.keys.should include(:id)
+    user.to_h.keys.should include(:name)
   end
 
   it 'shouldnt have unspecified data' do
-    user.to_h.keys.include?(:credit_card).should be_false
-    user.to_h.keys.include?(:secret_token).should be_false
+    user.to_h.keys.should_not include(:credit_card)
+    user.to_h.keys.should_not include(:secret_token)
   end
 
   it 'should change the hash representation when the attribute/method output changes' do
@@ -33,14 +37,14 @@ describe Hashifiable do
   end
 
   it 'should include procs in the hash representation' do
-    user.to_h.keys.include?(:two_times_two).should be_true
+    user.to_h.keys.should include(:two_times_two)
   end
 
   it 'should return the output of the function and not a proc' do
     user.to_h[:two_times_two].should == 4
   end
 
-  it 'should call the methods on the fly instead of just assignign whatever the result is at definition time' do
+  it 'should call the methods on the fly instead of just assigning whatever the result is at definition time' do
     user.to_h[:encrypted_token].should == user.secret_token + ' secret sauce'
 
     user.secret_token = 'NEW STUFF'
